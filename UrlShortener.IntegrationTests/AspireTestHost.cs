b23233c7cca665f5b830f855a221f34413719e43
Stefan Bradl <bradlstefan@googlemail.com>
2026-05-19 20:06:06 +0200

    integration test setup with example test

---
+++ UrlShortener.IntegrationTests/AspireTestHost.cs
@@ -0,0 +1,28 @@
+﻿using System.Threading.Tasks;
+using Aspire.Hosting;
+using Aspire.Hosting.Testing;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UrlShortener.IntegrationTests;
+
+[TestClass]
+public static class AspireTestHost
+{
+  public static DistributedApplication App { get; private set; } = default!;
+
+  [AssemblyInitialize]
+  public static async Task Start(TestContext context)
+  {
+    var builder = await DistributedApplicationTestingBuilder.CreateAsync<Projects.UrlShortener_AppHost>();
+
+    App = await builder.BuildAsync();
+
+    await App.StartAsync();
+  }
+
+  [AssemblyCleanup]
+  public static async Task Stop()
+  {
+    await App.StopAsync();
+  }
+}
--- UrlShortener.IntegrationTests/Test1.cs
+++ UrlShortener.IntegrationTests/Test1.cs
@@ -1,11 +1,29 @@
-﻿namespace UrlShortener.IntegrationTests;
+﻿using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+using Aspire.Hosting.Testing;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UrlShortener.IntegrationTests;
 
 [TestClass]
 public sealed class Test1
 {
-  [TestMethod]
-  public void TestMethod1()
+  private HttpClient GetClient()
   {
+    return AspireTestHost.App.CreateHttpClient("shortener");
+  }
 
+  [TestMethod]
+  public async Task CreateUrl_ShouldReturnSuccess()
+  {
+    var client = GetClient();
+
+    var response = await client.PostAsJsonAsync("/api/urls", new
+    {
+      url = "https://example.com"
+    });
+
+    Assert.IsTrue(response.IsSuccessStatusCode);
   }
 }
